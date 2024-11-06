a2b83d0249bbe92d8ae089e813bc5004aaba933d
Darwin Charris <98895036+DarwinCharris@users.noreply.github.com>
2024-11-06 00:35:26 -0500

    new functions

--- Functions/functions.js
+++ Functions/functions.js
@@ -129,13 +129,125 @@ function leftRecursion(gram){
     }
     return newgram
 }
-let str = 'E->E+T\r\nE->E-T\r\nE->T\r\nT->T*F\r\nT->T/F\r\nT->F\r\nF->(E)\r\nF->id'
-console.log(str)
-console.log(validate(str))
+function contarCifrasEnComun(lista) {
+    if (lista.length === 0) return 0;
+
+    // Tomamos el primer elemento como referencia
+    let referencia = lista[0];
+    let contador = 0;
+
+    // Recorremos cada carácter de la referencia
+    for (let i = 0; i < referencia.length; i++) {
+        // Verificamos si todos los elementos tienen el mismo carácter en la posición actual
+        if (lista.every(str => str[i] === referencia[i])) {
+            contador++;
+        } else {
+            break; // Sale del bucle al encontrar una diferencia
+        }
+    }
+    
+    return contador;
+}
+function factorization (gram){
+    let fact = new Gramatica()
+    //Para casa A de A-> X|Y|... uso de pivote a cada elemento de la producción y busco el patron a partir del primer carácter
+    for (let element of gram.rightPart){
+        let X=[] //No tienen patrón
+        let Y=[] // Tienen patrón
+        let production = element.productions
+        i = 0 
+        j = 1
+        while (i< production.length){
+            while (j< production.length){
+                if (production[i][0] === production[j][0]){
+                    Y.push(production[j])
+                }
+                j+=1
+            }
+            if(Y.length >0){
+                Y.unshift(production[i])
+                break //Hay patron, lo cual indica que hay recursividad (Solo existe un patron por producción)
+            }
+            i+=1
+            j=i+1
+        }
+
+        if(Y.length>0){
+            //Encontrar el patrón 
+            let salto = contarCifrasEnComun(Y)
+            let patron = Y[0].substring(0,salto)
+            for (let prod of Y){
+                //agregar la parte del patrón
+                fact.add(element.NTerm, `${patron}${element.NTerm}'`)
+                // agregar la parte sin patrón
+                if (prod.substring(salto) === ''){
+                    fact.add(`${element.NTerm}'`, '&')
+                }else{
+                    fact.add(`${element.NTerm}'`, prod.substring(salto))
+                }
+                
+            }
+            for (let solos of X){
+                fact.add(element.NTerm, solos)
+            }
+        }else{
+            for(let prod of production){
+                if (!Y.includes(prod)){
+                    fact.add(element.NTerm, prod)
+                }
+                
+            }
+        }
+
+    }
+    return fact
+}
+function newcomponents(gram){
+    let terminals =[]
+    let noTerminals = []
+    for (let nter of gram.rightPart){
+        if (!noTerminals.includes(nter.NTerm)){
+            noTerminals.push(nter.NTerm)
+        }
+        for(let prod of nter.productions){
+            i=0
+            while(i< prod.length){
+                if (prod[i]>= 'A' && prod[i] <= 'Z'){
+                    if (prod[i+1] === "'"){
+                        i=i+2
+                    }
+                    else{
+                        i=i+1
+                    }
+                }else{
+                    let termi =findTerminal(prod.substring(i))
+                    if(!terminals.includes(termi) && termi !='&' ){
+                        terminals.push(termi)
+                    }
+                    i = i+termi.length
+                }
+            }
+        }
+    }
+    return [terminals, noTerminals]
+}
+
+let str = 'S->Ab\r\nS->B\r\nA->Aa\r\nA->c\r\nA->d\r\nB->a\r\nB->aB'
+//console.log(str)
+//console.log(validate(str))
 let[terminales, noterminales, gramatica] = components(str)
-console.log(noterminales)
-console.log(terminales)
+//console.log(noterminales)
+//console.log(terminales)
 let nueva = leftRecursion(gramatica)
+let n2 = factorization(nueva)
 for (let elemento of nueva.rightPart){
     console.log(elemento)
+}
+//Factorizada
+let [terminales2, noterminales2 ]= newcomponents(n2)
+console.log(terminales2)
+console.log(noterminales2)
+console.log('Factorizada')
+for (let elementos of n2.rightPart){
+    console.log(elementos)
 }
